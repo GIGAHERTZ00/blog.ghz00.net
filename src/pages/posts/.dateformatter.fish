9a275a43f88bfcb3dd0fb526bc033b715ec142e6
GIGAHERTZ00 <gigahertz00@gmail.com>
2025-05-02 04:33:43 +0900

    更新日時の欄を追加

--- src/pages/posts/.dateformatter.fish
+++ src/pages/posts/.dateformatter.fish
@@ -4,4 +4,5 @@ if not string length -q $argv[1]
   return 1
 end
 
-sed -i "s/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}/$(date -r $argv[1] +%Y-%m-%dT%H:%M:%S)/" $argv[1]
+sed -i "s/updatedDate:  [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}/updatedDate:  $(date -r $argv[1] +%Y-%m-%dT%H:%M:%S)/" $argv[1]
+sed -i "s/updatedDate:  [0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}/updatedDate:  $(date -r $argv[1] +%Y-%m-%dT%H:%M:%S)/" $argv[1]
