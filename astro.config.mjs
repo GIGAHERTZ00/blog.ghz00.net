5290557dd19134a533abe79e84130daa9e6a8e40
GIGAHERTZ00 <gigahertz00@gmail.com>
2025-05-11 03:04:08 +0900

    kaTeXに対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.ghz00.net"
+  site: "https://blog.ghz00.net",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 });
